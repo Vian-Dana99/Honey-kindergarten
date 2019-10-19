e91ab6ce1aa8311356db765cdf2e84cc276d71bc
Vian-Dana99 <56548931+Vian-Dana99@users.noreply.github.com>
2019-10-19 13:37:35 +0300

    initial commit

---
+++ vika.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width = device-width , initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>The Honey Kindergarten | Project</title>
+<link rel="stylesheet" type="text/css" href="st.css">
+
+
+</head>
+<body itemscope itemtype="http://schema.org/Blog">
+
+<div class="wrapper">
+<div class="nav">	
+<div class="logo">
+Welcome to our kindergarten
+</div>
+
+<div class="menu">
+	<nav>
+
+   <ul>
+    <li><a href="home.html">Home</a></li>
+    <li><a href="sign.html">Sign up</a></li>
+    <li><a href="login.html">Login</a></li>
+   </ul>
+  </nav>
+
+	</ul>
+</div>
+</div>
+<div class="header">
+	<h1>Honey Kindergarten</h1>
+	<p>Kurdistan / Iraq</p>
+	 <p><a href="about.html">About</a> -
+     <a href="contact.html">Contact Us</a></p>
+</div>
+</div>
+ 
+
+</body>
+</html>
